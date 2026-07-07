10174719e10c83b0d2719d0db0f6ab15abb49350
Bryan Costanza <29235268+kj6dev@users.noreply.github.com>
2026-07-07 14:45:12 -0600

    chore(ios): build 23 (QR / deep-link setup)

--- ios/Project.swift
+++ ios/Project.swift
@@ -43,7 +43,7 @@ let project = Project(
                     "ASSETCATALOG_COMPILER_APPICON_NAME": "AppIcon",
                     "ASSETCATALOG_COMPILER_GLOBAL_ACCENT_COLOR_NAME": "AccentColor",
                     "MARKETING_VERSION": "0.1.0",
-                    "CURRENT_PROJECT_VERSION": "22",
+                    "CURRENT_PROJECT_VERSION": "23",
                 ],
                 configurations: [
                     .debug(name: "Debug", settings: [
