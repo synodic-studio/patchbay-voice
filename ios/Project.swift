9fa71e49e46cb5ddb3eca05b8896035088d440ed
Bryan Costanza <29235268+kj6dev@users.noreply.github.com>
2026-07-07 14:20:06 -0600

    chore(ios): build 22 (onboarding + Settings setup reference)

--- ios/Project.swift
+++ ios/Project.swift
@@ -36,7 +36,7 @@ let project = Project(
                     "ASSETCATALOG_COMPILER_APPICON_NAME": "AppIcon",
                     "ASSETCATALOG_COMPILER_GLOBAL_ACCENT_COLOR_NAME": "AccentColor",
                     "MARKETING_VERSION": "0.1.0",
-                    "CURRENT_PROJECT_VERSION": "21",
+                    "CURRENT_PROJECT_VERSION": "22",
                 ],
                 configurations: [
                     .debug(name: "Debug", settings: [
